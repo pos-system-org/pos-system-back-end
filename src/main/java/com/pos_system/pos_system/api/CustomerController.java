88ac15c96123d827b64b1fdbe345e8f789fcb9a3
MadhushanTharanga <madhushan0617@gmail.com>
2025-02-23 20:55:22 +0530

    updated!

--- src/main/java/com/pos_system/pos_system/api/CustomerController.java
+++ src/main/java/com/pos_system/pos_system/api/CustomerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/customers")
 @RequiredArgsConstructor
+@CrossOrigin
 public class CustomerController {
     private final CustomerService customerService;
     @PostMapping
